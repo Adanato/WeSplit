8da78e43eae3d5e84fc37da0632c31b0864f2b9f
Adam Nguyen <Adam0lifez@gmail.com>
2023-07-26 14:27:24 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adam Nguyen on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
